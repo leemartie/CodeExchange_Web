3d3c1d6309992ad4795017c4de41644a690fdb01
leemartie <lmartie@ics.uci.edu>
2014-07-22 17:18:26 -0700

    history now in reverse

--- WebContent/QueryTrail/QueryGridView.js
+++ WebContent/QueryTrail/QueryGridView.js
@@ -52,6 +52,7 @@ var QueryGridView = {
 
         return QueryGridView.grid;
     },
+//UPDATE
     update : function(){
 
         var htmlEscapes = {
@@ -75,112 +76,8 @@ var QueryGridView = {
 
 
         var lastCell = 0;
-        for(var i = 0; i<QueryGridModel.history.length; i++){
-            var stack = QueryGridModel.history[i];
-            var cellID = i%QueryGridView.size;
-            lastCell = cellID+1;
 
-            if(lastCell == 17)
-                lastCell = 0;
-
-            $(SetupManager.pound+"GridCell"+(cellID)).empty();
-
-            var table = $(SetupManager.tableOpen+SetupManager.tableClose);
-            table.addClass("QueryViewTable");
-            table.append($("<tr><th>session number: "+(i+1)+"</tr></th>"));
-            table.height("100%");
-            var row = $(SetupManager.trOpen+SetupManager.trClose);
-            var cell = $(SetupManager.tdOpen+SetupManager.tdClose);
-            cell.height("100%");
-            row.append(cell);
-
-
-            table.append(row);
-
-            for(var j = 0;j< stack.length;j++){
-                var row = $(SetupManager.trOpen+SetupManager.trClose);
-
-
-                var cell = $(SetupManager.tdOpen+SetupManager.tdClose);
-
-                var cellDiv = $(SetupManager.divOpen+SetupManager.divClose);
-
-
-
-                row.append(cell);
-
-                var query = stack[j];
-
-                if(query.displayValue instanceof Array){
-                    query.displayValue = query.displayValue[0].replace(/</gi,"&lt;");
-                    query.displayValue = query.displayValue.replace(/</gi,"&gt;");
-                }else if(!query.displayValue instanceof Boolean){
-                    query.displayValue = query.displayValue.replace(/</gi,"&lt;");
-                    query.displayValue = query.displayValue.replace(/</gi,"&gt;");
-                }
-
-                query.displayType = query.displayType.replace(/</gi,"&lt;");
-                query.displayType = query.displayType.replace(/</gi,"&gt;");
-
-                var label = $('<text><font color="darkred" size="2">['+query.displayType+']</font> ' +
-                    '<font color="black" size="2">'+escape(query.displayValue)+'</font></text>');
-
-                cell.append(label);
-
-//                var buttonCell = $(SetupManager.tdOpen+SetupManager.tdClose);
-//                var button = $(SetupManager.buttonOpen+SetupManager.buttonClose);
-//                buttonCell.append(button);
-//                button.addClass("QueryViewButton");
-//                button.attr("value","+");
-//                button.append($("<text>+</text>"));
-//                button.width("25px");
-//                button.height("15px");
-
-                cell.attr("title","click to add to current query");
-
-                (function(query, cell){cell.click(function(){
-                    BuildQueryBoxView.addAndSubmit(query);
-                    QueryGridView.added = true;
-                    UsageLogger.addEvent(UsageLogger.QUERY_HISTORY_CELL_PART_CLICK,query);
-                    $(SetupManager.pound+"gridButton").empty();
-                    $(SetupManager.pound+"gridButton").append("<text>show query history</text>");
-                    Controller.showGrid();
-                })}(query, cell));
-
-                //
-                (function(query,cell){cell.mouseenter(function(){
-
-                    cell.removeClass("GridRow");
-                    cell.addClass("GridRowHover");
-
-
-                })})(query,cell);
-//
-                (function(query,cell){cell.mouseleave(function(){
-
-
-                    cell.removeClass("GridRowHover");
-                    cell.addClass("GridRow");
-
-
-                })})(query,cell);
-
-   ///             row.append(buttonCell);
-
-                cell.width("100%");
-
-
-
-                cell.attr("valign","bottom");
-                cell.addClass("GridRow");
-                table.append(row);
-
-            }
-            $(SetupManager.pound+"GridCell"+(cellID)).append(table);
-
-
-        }
-//current contents
+        //current contents
 
         $(SetupManager.pound+"GridCell"+(lastCell)).empty();
 
@@ -188,35 +85,22 @@ var QueryGridView = {
         table.height("100%");
         table.addClass("QueryViewTable");
 
-
-
-
-
-        table.append($("<tr><th>session number: "+(lastCell+1)+"</tr></th>"));
+        table.append($("<tr><th>session number: "+( QueryGridModel.history.length+1)+"</th></tr>"));
         var row = $(SetupManager.trOpen+SetupManager.trClose);
 
         var cell = $(SetupManager.tdOpen+SetupManager.tdClose);
         cell.height("100%");
         row.append(cell);
 
-
         table.append(row);
 
         for(var i = 0; i<QueryBucketModel.stackOfQueries.length; i++){
-
-
-                var query = QueryBucketModel.stackOfQueries[i];
-
-
+            var query = QueryBucketModel.stackOfQueries[i];
             var row = $(SetupManager.trOpen+SetupManager.trClose);
-
             var cell = $(SetupManager.tdOpen+SetupManager.tdClose);
             cell.width("100%");
-
             row.append(cell);
-
             cell.attr("valign","bottom");
-
             if(query.displayValue instanceof Array){
                 query.displayValue = query.displayValue[0].replace(/</gi,"&lt;");
                 query.displayValue = query.displayValue.replace(/</gi,"&gt;");
@@ -225,24 +109,12 @@ var QueryGridView = {
                 query.displayValue = query.displayValue.replace(/</gi,"&gt;");
             }
 
-
-
             query.displayType = query.displayType.replace(/</gi,"&lt;");
             query.displayType = query.displayType.replace(/</gi,"&gt;");
 
             var label = $('<text><font color="darkred" size="2">['+query.displayType+']</font> ' +
                 '<font color="black" size="2">'+escape(query.displayValue)+'</font></text>');
             cell.append(label);
-
-
-//            var buttonCell = $(SetupManager.tdOpen+SetupManager.tdClose);
-//            var button = $(SetupManager.buttonOpen+SetupManager.buttonClose);
-//            buttonCell.append(button);
-//            button.addClass("QueryViewButton");
-//            button.attr("value","+");
-//            button.append($("<text>+</text>"));
-//            button.width("25px");
-//            button.height("15px");
 
             cell.attr("title","click to add to current query");
             (function(query,cell){cell.click(function(){
@@ -266,31 +138,120 @@ var QueryGridView = {
             (function(query,cell){cell.mouseleave(function(){
 
 
-                    cell.removeClass("GridRowHover");
-                    cell.addClass("GridRow");
+                cell.removeClass("GridRowHover");
+                cell.addClass("GridRow");
 
 
             })})(query,cell);
 
-  //          row.append(buttonCell);
-
             cell.addClass("GridRow");
-
-
             table.append(row);
-
-
 
         }
 
         table.addClass("GridCellHighLight");
-
         $(SetupManager.pound+"GridCell"+(lastCell)).append(table);
 
 //end current query
 
 
-    }
+
+//the rest of the history
+        if(QueryGridModel.history != null) {
+            var cellCount = 1;
+            for (var i = QueryGridModel.history.length-1; i >= 0 ; i--) {
+                var stack = QueryGridModel.history[i];
+                var cellID = (cellCount) % QueryGridView.size;
+                cellCount++;
+                lastCell = cellID + 1;
+
+                if (lastCell == 17)
+                    lastCell = 0;
+
+                $(SetupManager.pound + "GridCell" + (cellID)).empty();
+
+                var table = $(SetupManager.tableOpen + SetupManager.tableClose);
+                table.addClass("QueryViewTable");
+                table.append($("<tr><th>session number: " + (i+1) + "</th></tr>"));
+                table.height("100%");
+                var row = $(SetupManager.trOpen + SetupManager.trClose);
+                var cell = $(SetupManager.tdOpen + SetupManager.tdClose);
+                cell.height("100%");
+                row.append(cell);
+
+
+                table.append(row);
+
+                for (var j = 0; j < stack.length; j++) {
+                    var row = $(SetupManager.trOpen + SetupManager.trClose);
+                    var cell = $(SetupManager.tdOpen + SetupManager.tdClose);
+                    var cellDiv = $(SetupManager.divOpen + SetupManager.divClose);
+                    row.append(cell);
+                    var query = stack[j];
+
+                    if (query.displayValue instanceof Array) {
+                        query.displayValue = query.displayValue[0].replace(/</gi, "&lt;");
+                        query.displayValue = query.displayValue.replace(/</gi, "&gt;");
+                    } else if (!query.displayValue instanceof Boolean) {
+                        query.displayValue = query.displayValue.replace(/</gi, "&lt;");
+                        query.displayValue = query.displayValue.replace(/</gi, "&gt;");
+                    }
+
+                    query.displayType = query.displayType.replace(/</gi, "&lt;");
+                    query.displayType = query.displayType.replace(/</gi, "&gt;");
+
+                    var label = $('<text><font color="darkred" size="2">[' + query.displayType + ']</font> ' +
+                        '<font color="black" size="2">' + escape(query.displayValue) + '</font></text>');
+
+                    cell.append(label);
+                    cell.attr("title", "click to add to current query");
+
+                    (function (query, cell) {
+                        cell.click(function () {
+                            BuildQueryBoxView.addAndSubmit(query);
+                            QueryGridView.added = true;
+                            UsageLogger.addEvent(UsageLogger.QUERY_HISTORY_CELL_PART_CLICK, query);
+                            $(SetupManager.pound + "gridButton").empty();
+                            $(SetupManager.pound + "gridButton").append("<text>show query history</text>");
+                            Controller.showGrid();
+                        })
+                    }(query, cell));
+
+                    //
+                    (function (query, cell) {
+                        cell.mouseenter(function () {
+
+                            cell.removeClass("GridRow");
+                            cell.addClass("GridRowHover");
+
+
+                        })
+                    })(query, cell);
+//
+                    (function (query, cell) {
+                        cell.mouseleave(function () {
+
+
+                            cell.removeClass("GridRowHover");
+                            cell.addClass("GridRow");
+
+
+                        })
+                    })(query, cell);
+
+                    cell.width("100%");
+                    cell.attr("valign", "bottom");
+                    cell.addClass("GridRow");
+                    table.append(row);
+
+                }
+                $(SetupManager.pound + "GridCell" + (cellID)).append(table);
+
+
+            }//for
+        }
+
+    }//update
 
 
 }
