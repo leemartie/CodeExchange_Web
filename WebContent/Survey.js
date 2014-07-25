998f034155aea68cd57f251ef22f5c349e003614
leemartie <lmartie@ics.uci.edu>
2014-07-24 17:15:19 -0700

    can get infrom from survey now

--- WebContent/Survey.js
+++ WebContent/Survey.js
@@ -3,7 +3,7 @@
  */
 var Survey = {
 
-
+    questionCells : new Array(),
 
 
     getView : function(){
@@ -27,14 +27,17 @@ var Survey = {
 
 
         var cell1 = new questionCell("Did you find useful code?",null,"boolean",1);
-        subRow.append(cell1);
-        cell1.attr("height","10%");
+        subRow.append(cell1.cell);
+        cell1.cell.attr("height","10%");
+        Survey.questionCells.push(cell1);
 
         var subRow = $(SetupManager.trOpen+SetupManager.trClose);
         var cell9 = new questionCell("Did you find autocomplete useful?",
             'http://codeexchange.ics.uci.edu/autocomplete.png',"boolean",9,200);
-        cell9.attr("height","90%");
-        subRow.append(cell9);
+        cell9.cell.attr("height","90%");
+        Survey.questionCells.push(cell9);
+
+        subRow.append(cell9.cell);
         subTable.append(subRow);
         subTable.attr("height","100%");
         subTable.css({"margin":"0", "border":"0", "border-spacing":"0", "padding":"0"});
@@ -58,9 +61,9 @@ var Survey = {
 
         var cell2 = new questionCell("Did you find code criticisms useful?",
             'http://codeexchange.ics.uci.edu/critisims.png',"boolean",2,150);
-        subRow.append(cell2);
-        cell2.attr("height","50%");
-
+        subRow.append(cell2.cell);
+        cell2.cell.attr("height","50%");
+        Survey.questionCells.push(cell2);
 
 
 //cell 11
@@ -68,22 +71,22 @@ var Survey = {
 
         var cell11 = new questionCell("Did you find keywords useful?",
             'http://codeexchange.ics.uci.edu/keywords.png',"boolean",11,250);
-        cell11.attr("height","143px");
-        subRow.append(cell11);
+        cell11.cell.attr("height","143px");
+        subRow.append(cell11.cell);
         subTable.append(subRow);
         subTable.attr("height","100%");
         subTable.css({"margin":"0", "border":"0", "border-spacing":"0", "padding":"0"});
-
+        Survey.questionCells.push(cell11);
 //cell 3
         var cell3 = new questionCell("Did you find code properties useful?",
             'http://codeexchange.ics.uci.edu/clickable_properties.png',"boolean",3,250);
-        row1.append(cell3);
-
+        row1.append(cell3.cell);
+        Survey.questionCells.push(cell3);
 
         var cell7 = new questionCell("Did you find history useful?",
             'http://codeexchange.ics.uci.edu/History.png',"boolean",6,350);
-        row1.append(cell7);
-
+        row1.append(cell7.cell);
+        Survey.questionCells.push(cell7);
 
 
 
@@ -93,17 +96,19 @@ var Survey = {
         surveyView.append(row2);
         var cell4 = new questionCell("Did you find recommendations useful?",
             'http://codeexchange.ics.uci.edu/Recommendations.png',"boolean",4,200);
-        row2.append(cell4);
-
+        row2.append(cell4.cell);
+        Survey.questionCells.push(cell4);
 
 //cell 5
         var cell5 = new questionCell("Did you find advanced search useful?",
             'http://codeexchange.ics.uci.edu/AdvancedSearch.png',"boolean",5,200);
-        row2.append(cell5);
+        row2.append(cell5.cell);
+        Survey.questionCells.push(cell5);
 //cell8
         var cell8 = new questionCell("Did you find the current query useful?",
             'http://codeexchange.ics.uci.edu/CurrentQuery.png',"boolean",8,200);
-        row2.append(cell8);
+        row2.append(cell8.cell);
+        Survey.questionCells.push(cell8);
 
 //cell10
         var subTable = $(SetupManager.tableOpen+SetupManager.tableClose);
@@ -116,16 +121,16 @@ var Survey = {
 
         var cell10 = new questionCell("Did you find pages useful?",
             'http://codeexchange.ics.uci.edu/pageNumbers.png',"boolean",5,300);
-        subRow.append(cell10);
+        subRow.append(cell10.cell);
         subTable.append(subRow);
-
+        Survey.questionCells.push(cell10);
 //cell 6
         var subRow = $(SetupManager.trOpen+SetupManager.trClose);
         var cell6 = new questionCell("Any other feedback?",
             null,"open",6);
-        subRow.append(cell6);
+        subRow.append(cell6.cell);
         subTable.append(subRow);
-
+        Survey.questionCells.push(cell6);
 
 
 
@@ -202,7 +207,18 @@ var Survey = {
         });
 
         submitButton.click(function(event){
+            var result = "";
+            for(var i = 0; i < Survey.questionCells.length; i++){
+                var question = Survey.questionCells[i].question;
+                var answer = Survey.questionCells[i].answer;
 
+                if(i == 0)
+                    result = result + question +"\t"+answer;
+                else
+                    result = "\n"+result + question +"\t"+answer;
+            }
+
+            alert(result);
         });
 
 //div
@@ -229,6 +245,9 @@ var Survey = {
 }
 
 function questionCell(question, imageURL, type, questionNumber, width){
+    this.question = question;
+    this.answer = null;
+    this.cell = null;
 
     if(type == "boolean"){
 
@@ -249,7 +268,14 @@ function questionCell(question, imageURL, type, questionNumber, width){
         this.yesBox.attr("name","boolean"+questionNumber);
         this.yesBox.attr("value","yes");
         this.yesBox.attr("id", "radio1");
+        this.yesBox.attr("height", "50");
         this.yesLabel.css({"background-color":"lightblue", "border-radius":"25px", "padding":"5px"});
+        var is = this;
+        this.yesBox.click(function(event){
+                is.yesLabel.addClass("BoxSelected");
+                is.falseBoxLabel.removeClass("BoxSelected");
+                is.answer = "yes";
+        });
 
         this.falseBox = $(SetupManager.inputOpen+SetupManager.inputClose);
         this.falseBoxLabel = $("<text>no</text>");
@@ -260,13 +286,18 @@ function questionCell(question, imageURL, type, questionNumber, width){
         this.falseBox.attr("value","no");
         this.falseBox.attr("id", "radio2");
         this.falseBoxLabel.css({"background-color":"lightblue", "border-radius":"25px", "padding":"5px"});
+        this.falseBox.click(function(event){
+            is.yesLabel.removeClass("BoxSelected");
+            is.falseBoxLabel.addClass("BoxSelected");
+            is.answer = "no";
+        });
 
         this.cell.append($("<p></p>"));
         if(imageURL != null) {
             this.cell.append("<img width='"+width+"' height='auto' src='" + imageURL + "'></img>");
         }
 
-        return this.cell;
+
 
     }else if(type == "open"){
         this.cell = $(SetupManager.tdOpen+SetupManager.tdClose);
@@ -287,7 +318,15 @@ function questionCell(question, imageURL, type, questionNumber, width){
 
         this.textBox.attr("id", "textboxfeedback");
 
-        return this.cell;
+        var is = this;
+        this.textBox.keypress(function(event){
+            is.answer = is.textBox.val();
+        });
+
+
+
+
+
     }
 
 
